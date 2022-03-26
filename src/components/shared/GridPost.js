dfd33e555f6b76abed2f4f10bfedf1347bbd09dd
JiongnanLiu <jia7833@hotmail.com>
2022-03-26 00:47:37 -0400

    add explore follow suggestions grid and explore post grid

--- src/components/shared/GridPost.js
+++ src/components/shared/GridPost.js
@@ -1,10 +1,26 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
 import { useGridPostStyles } from "../../styles";
 
-function GridPost() {
-  useGridPostStyles();
+function GridPost({ post }) {
+  const classes = useGridPostStyles();
+  const { media, comments, likes } = post
 
-  return <div>GridPost</div>;
+  return (
+    <div className={classes.gridPostContainer}>
+      <div className={classes.gridPostOverlay}>
+        <div className={classes.gridPostInfo}>
+          <span className={classes.likes} />
+          <Typography>{likes}</Typography>
+        </div>
+        <div className={classes.gridPostInfo}>
+          <span className={classes.comments} />
+          <Typography>{comments.length}</Typography>
+        </div>
+      </div>
+      <img src={media} alt="Post cover" className={classes.image} />
+    </div>
+  )
 }
 
 export default GridPost;
--- src/pages/explore.js
+++ src/pages/explore.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Layout from "../components/shared/Layout";
+import ExploreSuggestions from "../components/explore/ExploreSuggestions";
+import ExploreGrid from "../components/explore/ExploreGrid";
 
 function ExplorePage() {
   return (
     <Layout>
-      explore
+      <ExploreSuggestions />
+      <ExploreGrid />
     </Layout>
   )
 }
