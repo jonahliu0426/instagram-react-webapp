20c77c8fe4f80b6d0dc7196ec82e726fbb44cf58
JiongnanLiu <jia7833@hotmail.com>
2022-03-26 18:55:35 -0400

    add profile user info and bio section

--- src/components/shared/ProfilePicture.js
+++ src/components/shared/ProfilePicture.js
@@ -1,10 +1,27 @@
+import { Person } from "@material-ui/icons";
 import React from "react";
 import { useProfilePictureStyles } from "../../styles";
 
-function ProfilePicture() {
-  useProfilePictureStyles();
+function ProfilePicture({
+  size,
+  isOwner,
+  image = "https://reedbarger.nyc3.digitaloceanspaces.com/reactbootcamp/avatar.png"
+}) {
+  const classes = useProfilePictureStyles({ size, isOwner });
 
-  return <div>ProfilePicture</div>;
+  return (
+    <section className={classes.section}>
+      {image ? (
+        <div className={classes.wrapper}>
+          <img src={image} alt="user profile" className={classes.image} />
+        </div>
+      ) : (
+        <div className={classes.wrapper}>
+          <Person className={classes.person} />
+        </div>
+      )}
+    </section>
+  )
 }
 
 export default ProfilePicture;
